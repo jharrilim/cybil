ec3980a97513a1c14c8a175d1d75c4d83217e4eb
Joseph Harrison-Lim <Josephharrisonlim@gmail.com>
2019-04-20 18:09:07 -0400

    Add more activation funcs

--- lib/core/src/activation.rs
+++ lib/core/src/activation.rs
@@ -1,9 +1,37 @@
+use nalgebra::max;
+
 pub fn sigmoid(a: f32) -> f32 {
     1f32 / (1f32 + a.powf(-1f32))
 }
 
 pub fn sigmoid_prime(a: f32) -> f32 {
     sigmoid(a) * (1f32 - sigmoid(a))
+}
+
+pub fn tanh(a: f32) -> f32 {
+    a.tanh()
+}
+
+pub fn tanh_prime(a: f32) -> f32 {
+    let tanh_a = a.tanh();
+
+    1 - tanh_a * tanh_a
+}
+
+pub fn relu(a: f32) -> f32 {
+    max(0f32, a)
+}
+
+pub fn relu_prime (a: f32) -> f32 {
+    if a <= 0f32 { 0f32 } else { 1f32 }
+}
+
+pub fn linear(a: f32) -> f32 {
+    a
+}
+
+pub fn linear_prime(a: f32) -> f32 {
+    1f32
 }
 
 
